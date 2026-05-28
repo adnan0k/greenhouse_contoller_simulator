afabc199038f21e0cdee7db1c4643b2c83c8749e
Wania Amir <108606063+Wania11@users.noreply.github.com>
2026-05-28 16:24:36 +0500

    Extracting environment data from the stream and storing it into relevant variables.

--- Controller.h
+++ Controller.h
@@ -36,6 +36,17 @@ public:
 
     ControllerOutputs systemController(EnvironmentalData data) {
         ControllerOutputs outputs = {false, false, false, false};
+        
+        // Environment Data Extraction
+        // Check for error (bit 15)
+        uint16_t checkError = (data.Data & 0x8000) >> 15;
+        if (checkError) {
+            state = EMERGENCY_STOP;
+        }
+
+        int moisture = (data.Data >> 0) & 0xF; // Bits 0-3
+        int temperature = (data.Data >> 4) & 0x7F; // Bits 4-10
+        int sunlight = (data.Data >> 11) & 0x1FFF; // Bits 11-24
 
         return outputs;
     }
